81985b636b9df65c578cd9d0790bc41acc9e8b7c
d4hy <davidhoang253@gmail.com>
2025-06-11 00:25:55 -0700

    need to implement contact message

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { NotFound } from "./pages/NotFound"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { NotFound } from "./pages/NotFound";
 import { ThemeProvider } from "@/components/ThemeToggle";
-import {Projects} from "./pages/Projects";
+import { Projects } from "./pages/Projects";
 import { Home } from "./pages/Home";
+import { Toaster } from "./components/ui/toaster";
 function App() {
   return (
     <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-           <Route path="/projects" element={<Projects />}/>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <Toaster>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </Toaster>
     </ThemeProvider>
   );
 }
 
-
-export default App
+export default App;

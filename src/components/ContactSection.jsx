6b082d3b7a7d3d9ecc46a6bcab10d955b0584fce
d4hy <davidhoang253@gmail.com>
2025-06-21 01:31:04 -0700

    trying to integrate email js

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -2,10 +2,15 @@ import { Linkedin, Mail, MapPin, Phone, Send } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { useToast } from "@/hooks/use-toast";
 import { useState } from "react";
-
+import emailjs from "emailjs-com";
 export const ContactSection = () => {
   const { toast } = useToast();
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,6 +22,23 @@ export const ContactSection = () => {
         title: "Message sent!",
         description: "Thank you for your message. I'll get back to you soon.",
       });
+      /* A Promise is a special JavaScript object used to handle asynchronous operations which sendForm returns.
+       * Both then and and async are used to handle promises.
+        .then is used for chaining while async makes it look synchronous.
+       */
+       emailjs
+      .sendForm(
+        import.meta.env.VITE_SERVICE_ID,
+        import.meta.env.VITE_TEMPLATE_ID,
+        e.target,
+        import.meta.env.VITE_PUBLIC_KEY
+      )
+      .then((result) => {
+        alert("Message Sent!");
+        setFormData({ name: "", email: "", message: "" });
+      })
+      .catch(() => alert("Oops! Something went wrong. Please try again."));
+
       setIsSubmitting(false);
     }, 1500);
   };
@@ -87,7 +109,11 @@ export const ContactSection = () => {
                   {" "}
                   Your Name
                 </label>
-                <input type="text" id="name" name="name" required className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary" placeholder="David Hoang..." />
+                <input type="text" id="name" name="name" required value ={formData.name} className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary" placeholder="David Hoang..."
+                onChange={(e) =>
+                  setFormData({ ...formData, name: e.target.value })
+                }  
+                />
               </div>
 
               <div>
@@ -95,7 +121,10 @@ export const ContactSection = () => {
                   {" "}
                   Your Email
                 </label>
-                <input type="email" id="email" name="email" required className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary" placeholder="john@gmail.com" />
+                <input type="email" id="email" name="email" required value ={formData.email} className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary" placeholder="john@gmail.com"
+                onChange={(e) =>
+                  setFormData({ ...formData, email: e.target.value })
+                } />
               </div>
 
               <div>
@@ -103,7 +132,11 @@ export const ContactSection = () => {
                   {" "}
                   Your Message
                 </label>
-                <textarea id="message" name="message" required className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary resize-none" placeholder="Hello, I'd like to talk about..." />
+                <textarea id="message" name="message" required value ={formData.message} className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden focus:ring-2 focus:ring-primary resize-none" placeholder="Hello, I'd like to talk about..."
+                onChange={(e) =>
+                  setFormData({ ...formData, message: e.target.value })
+                }
+                />
               </div>
 
               <button type="submit" disabled={isSubmitting} className={cn("cosmic-button w-full flex items-center justify-center gap-2")}>
